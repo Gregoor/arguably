ab101f001224164f7f674d047ba2e40ce0ff64fb
Gregor Weber <gregorwbr@gmail.com>
2017-03-01 02:33:13 +0100

    use babel-relay-plugin to make heroku deploy work

---
+++ scripts/babel-relay-plugin.js
@@ -0,0 +1,6 @@
+const getBabelRelayPlugin = require('babel-relay-plugin');
+
+const schemaData = require('../data/schema.json');
+
+
+module.exports = getBabelRelayPlugin(schemaData.data);
---
+++ scripts/build-schema.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+
+const {graphql}  = require('graphql');
+const {introspectionQuery, printSchema} = require('graphql/utilities');
+
+const schema = require('../server/graphql');
+
+
+const schemaPath = path.join(__dirname, '..', 'data');
+
+fs.mkdirSync(schemaPath);
+
+const schemaFilePath = path.join(schemaPath, 'schema');
+
+fs.writeFileSync(
+  `${schemaFilePath}.graphql`,
+  printSchema(schema)
+);
+
+graphql(schema, introspectionQuery).then((result) => {
+  fs.writeFileSync(
+    `${schemaFilePath}.json`,
+    JSON.stringify(result, null, 2)
+  );
+  process.exit();
+});
